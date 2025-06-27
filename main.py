e24011b2c8ead0936d217c1de37137e4c786dce3
Nico-DM <nicomgaletto@gmail.com>
2025-06-27 21:12:51 +0000

    seguridad

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from serpapi import GoogleSearch
+from api_key import API_KEY
 
 def main():
     params = {
-    "engine": "google_lens",
-    "url": "https://i.imgur.com/HBrB8p0.png",
-    "api_key": "ded253e555b914cd3e4a74f5a08fef21dfda4a119ddb75167967ed3d66b81328"
+        "engine": "google_lens",
+        "type": "exact_matches",
+        "url": "https://i.imgur.com/HBrB8p0.png",
+        "api_key": API_KEY
     }
 
     search = GoogleSearch(params)
     results = search.get_dict()
-    visual_matches = results["visual_matches"]
-    print(visual_matches)
+    exact_matches = results["exact_matches"]
+    print(exact_matches)
 
 if __name__ == "__main__":
     main()
