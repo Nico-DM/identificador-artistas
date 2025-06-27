563eddff03b651ed32504bfc33a58730aa8dc0cd
Nico-DM <nicomgaletto@gmail.com>
2025-06-27 21:35:17 +0000

    modified main to not waste API searches

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from serpapi import GoogleSearch
 from api_key import API_KEY
+import json
+
+def save_example(dictionary):
+    with open("example.json", "w") as f:
+        json.dump(dictionary, f, indent=4)
+
+def read_example():
+    with open("example.json", "r") as f:
+        result = json.loads(f.read())
+    
+    return result
 
 def main():
-    params = {
-        "engine": "google_lens",
-        "type": "exact_matches",
-        "url": "https://i.imgur.com/HBrB8p0.png",
-        "api_key": API_KEY
-    }
+    try:
+        exact_matches = read_example()
+    except FileNotFoundError:
+        params = {
+            "engine": "google_lens",
+            "type": "exact_matches",
+            "url": "https://i.imgur.com/HBrB8p0.png",
+            "api_key": API_KEY
+        }
 
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    exact_matches = results["exact_matches"]
-    print(exact_matches)
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        exact_matches = results["exact_matches"]
+        save_example(exact_matches)
+
+    #print(json.dumps(exact_matches, indent=4))
 
 if __name__ == "__main__":
     main()
